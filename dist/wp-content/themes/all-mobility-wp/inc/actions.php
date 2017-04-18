18a501d36038ab1f77700db213e5822f3863d28c
aleksandrnikolajcuk <deathstars111>
2017-04-18 16:57:09 +0300

    Core

--- dist/wp-content/themes/all-mobility-wp/inc/actions.php
+++ dist/wp-content/themes/all-mobility-wp/inc/actions.php
@@ -60,7 +60,7 @@ function add_js()
     wp_register_script('swiper_js',get_template_directory_uri().'/assets/js/vendors/swiper.jquery.min.js');
     
     wp_register_script('index_js',get_template_directory_uri().'/assets/js/index.min.js');
-    wp_register_script('perfect_js',get_template_directory_uri().'/assets/js/vendor/perfect-scrollbar.jquery.min.js');
+    wp_register_script('perfect_js',get_template_directory_uri().'/assets/js/vendors/perfect-scrollbar.jquery.min.js');
     wp_register_script('product_js',get_template_directory_uri().'/assets/js/product.min.js');
 
     wp_enqueue_script('jquery');
@@ -269,120 +269,7 @@ function theme_pagination() {
 
 function get_posts_current(){
 
-    $json_data='';
-    $cur='';
-    $paged=$_GET['currentPage']+1;
-    $param = $_GET['filterData'];
-    parse_str($param);
 
-    if($k1){
-
-        $range_1 = array(
-            'key' => 'project_footage',
-            'value' => array(100,1000),
-            'compare' => 'BETWEEN',
-            'type' => 'NUMERIC',
-        );
-
-    }
-    if($k3){
-        $range_2 = array(
-            'key' => 'project_footage',
-            'value' => array(1000,3000),
-            'compare' => 'BETWEEN',
-            'type' => 'NUMERIC',
-        );
-
-    }
-    if($k6){
-
-        $range_3 = array(
-            'key' => 'project_footage',
-            'value' => array(3000,6000),
-            'compare' => 'BETWEEN',
-            'type' => 'NUMERIC',
-        );
-
-    }
-    if($k15){
-
-        $range_4 = array(
-            'key' => 'project_footage',
-            'value' => array(6000,999999),
-            'compare' => 'BETWEEN',
-            'type' => 'NUMERIC',
-        );
-
-    }
-
-    $args = array(
-        'paged' => $paged,
-        'post_type' => 'project',
-        'posts_per_page' => '6',
-        'post_status' => 'publish',
-        'meta_query' => array(
-            'relation' => 'OR',
-            $range_1,
-            $range_2,
-            $range_3,
-            $range_4,
-        ),
-    );
-    $projects = new WP_query ( $args );
-    $max = $projects->max_num_pages;
-
-    if ( $projects->have_posts() ) {
-
-        $json_data='';
-        $cur='';
-        while ( $projects->have_posts()) :
-
-            $col = 10;
-            $projects->the_post();
-
-            $thumb_id = get_post_thumbnail_id();
-            $thumb_url = wp_get_attachment_image_src($thumb_id,'full')[0];
-            $current_icon_footage = get_field('project_footage');
-            if($current_icon_footage <= 1000){
-                $cur_footage = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 46'><path class='cls-1' d='M36.17,29.87L24.69,24.05V18a0.3,0.3,0,0,0-.16-0.26L13.08,11.33a0.31,0.31,0,0,0-.31,0,0.3,0.3,0,0,0-.15.26V30.27a0.27,0.27,0,0,0,0,.08,0.3,0.3,0,0,0,0,.12,0.3,0.3,0,0,0,.16.27l11.46,6.44a0.31,0.31,0,0,0,.15,0,0.3,0.3,0,0,0,.15,0L36.18,30.4A0.3,0.3,0,0,0,36.17,29.87ZM24.08,18.21v5.85L13.24,30V12.11Zm0,6.54V36.38L13.56,30.47Zm0.61,11.62V24.74l10.7,5.42Z'/></svg>";
-            }
-            elseif($current_icon_footage <= 3000){
-                $cur_footage = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 46'><path class='cls-1' d='M35.64,21.8a0.31,0.31,0,0,0-.17-0.26l-7.74-4,7.71-4.37a0.31,0.31,0,0,0,0-.54L24,6.37a0.31,0.31,0,0,0-.29,0L12.12,12.66a0.31,0.31,0,0,0-.16.27,0.3,0.3,0,0,0,.06.18v8.82a0.27,0.27,0,0,0,0,.41v9.57a0.31,0.31,0,0,0,.15.26l11.46,6.94a0.3,0.3,0,0,0,.16,0,0.31,0.31,0,0,0,.31-0.31V28.76l11.4-6.69A0.31,0.31,0,0,0,35.64,21.8ZM23.82,7l10.85,5.95-7.59,4.3-3.25-1.67a0.31,0.31,0,0,0-.29,0l-3,1.66L12.9,12.94Zm-3.3,10.94,3,1.67v8.57l-10.61-6Zm-7.88,3.63V13.49l7.25,4.09Zm11.18-2.48-2.66-1.5,2.54-1.4,2.75,1.41ZM12.64,31.74v-9l10.84,6.16v9.44Zm11.46-3.69V19.64l3-1.7,7.6,3.89Z'/></svg>";
-            }
-            elseif($current_icon_footage <= 6000){
-                $cur_footage = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 46'><path class='cls-1' d='M34.7,14.6a0.29,0.29,0,0,0,0,0,0.31,0.31,0,0,0-.16-0.27l-2.93-1.6,2.68-1.56a0.31,0.31,0,0,0,0-.54L22.63,4.65a0.31,0.31,0,0,0-.29,0L10.88,10.92a0.31,0.31,0,0,0,0,.54L13.6,13l-2.19,1.19a0.29,0.29,0,0,0-.13-0.05,0.31,0.31,0,0,0-.31.31v13.3a0.31,0.31,0,0,0,.16.27l11.44,6.25v6.33a0.31,0.31,0,0,0,.45.27l11.68-6.38a0.31,0.31,0,0,0,.16-0.27V14.85A0.29,0.29,0,0,0,34.7,14.6Zm-23-3.4L22.5,5.27l11,5.62L31,12.35,23.06,8a0.31,0.31,0,0,0-.29,0l-8.54,4.63Zm18.72,1.52-7.89,4.61L14.86,13l8.05-4.37Zm-16.15.64,8.12,4.59a0.31,0.31,0,0,0,.15,0,0.3,0.3,0,0,0,.15,0L31,13.06l2.77,1.51L22.92,20.7,12,14.57Zm-2.64,14.2V15l11,6.17V33.56Zm11.6,12.52V34.31l0,0a0.29,0.29,0,0,0,0-.31V21.25L34.26,15V34Z'/></svg>";
-            }
-            elseif($current_icon_footage > 6000){
-                $cur_footage = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 46 46'><path class='cls-1' d='M37.34,10.76L25.9,4.46a0.3,0.3,0,0,0-.29,0l-17.13,9a0.31,0.31,0,0,0-.13.41h0a0.31,0.31,0,0,0-.15.26V27.51a0.31,0.31,0,0,0,.16.27L20,34.19a0.31,0.31,0,0,0,.15,0,0.3,0.3,0,0,0,.16,0,0.31,0.31,0,0,0,.15-0.26V33.84L22,32.89V41.7a0.31,0.31,0,0,0,.15.26,0.3,0.3,0,0,0,.3,0l11.62-6.41a0.31,0.31,0,0,0,.16-0.27V15.86a0.31,0.31,0,0,0-.15-0.26,0.3,0.3,0,0,0-.3,0L22.47,21.83l-2.06-1.06v-0.2a0.3,0.3,0,0,0,0-.09l17-9.17A0.31,0.31,0,0,0,37.34,10.76ZM19.79,33.4l-10.9-6,10.9-6v12Zm-11-6.65V14.68l10.95,6ZM33.64,16.38V35.11l-11,6.07V32.53l9.13-5.39a0.31,0.31,0,0,0,0-.54l-8.61-4.43ZM31,26.9l-8.34,4.92V22.6Zm-9-4.61v9.89l-1.61.95V21.46ZM20,20a0.29,0.29,0,0,0-.09.12L8.81,14,25.75,5.08,36.55,11Z'/></svg>";
-            }
-
-            ?>
-
-            <?php   $cur .= '{
-            "pic": "'.$thumb_url.'",
-            "name": "'.get_the_title().'",
-            "icon": "'.$cur_footage.'",
-            "square": "'.get_field("project_footage").'",
-            "link": "'.get_the_permalink().'"
-            },';
-            ?>
-
-        <?php endwhile;
-    }
-    if($max==$paged){
-        $col = 0;
-    }
-    $cur = substr($cur, 0, -1);
-
-    $json_data = '{
-                "col": '.$col.',
-                "items": [
-                  '.$cur.'
-               ]
-              }';
-
-    echo $json_data;
-    exit;
 }
 
 add_action('wp_ajax_get_posts','get_posts_current');
@@ -395,3 +282,69 @@ remove_action('woocommerce_before_main_content','woocommerce_output_content_wrap
 remove_action('woocommerce_before_main_content','woocommerce_breadcrumb',20);
 remove_action('woocommerce_after_main_content','woocommerce_output_content_wrapper_end',10);
 remove_action('woocommerce_get_sidebar','woocommerce_sidebar',10);
+
+add_action('wb_woocommerce_single_product_summary','woocommerce_template_single_title', 5);
+add_action('wb_woocommerce_single_product_summary','woocommerce_template_single_rating', 10);
+add_action('wb_woocommerce_single_product_summary','woocommerce_template_single_excerpt', 20);
+add_action('wb_slider_preview','get_preview_slider', 5);
+
+function wb_get_content (){
+    echo get_the_content();
+}
+
+function get_preview_slider(){
+
+    global $product;
+    
+    $attachment_ids = $product->get_gallery_attachment_ids();
+    $thumb_id = get_post_thumbnail_id();
+    $thumb_url = wp_get_attachment_image_src($thumb_id,'full')[0];
+    $image_urls[] = $thumb_url;
+
+    foreach ( $attachment_ids as $attachment_id){
+        $url = wp_get_attachment_image_src( $attachment_id, 'full' )[0];
+        $image_urls[] = $url;
+    }
+
+    ?>
+
+    <?php 
+    if(!empty($attachment_ids)): ?>
+
+        <!-- product__slider -->
+        <div class="product__slider">
+
+        <div class="swiper-container gallery-top">
+        <div class="swiper-wrapper">
+    
+    <?php foreach ($image_urls as $image_url) { ?>
+
+        <div class="swiper-slide" style="background-image:url(<?= $image_url ?>)"></div>
+        
+   <?php  }  ?>
+        </div></div>
+          <div class="swiper-container gallery-thumbs">
+                            <div class="swiper-wrapper">
+            <?php
+
+        foreach ($image_urls as $image_url) { ?>
+            <div class="swiper-slide">
+                <div class="product__slider-thumbs" style="background-image:url(<?= $image_url ?>)"></div>
+            </div>
+        <?php }
+        ?>
+        </div></div></div>
+        
+   <?php  endif;?>
+
+<?php }
+
+add_filter( 'woocommerce_get_price_html', 'custom_price_html', 100, 2 );
+
+function custom_price_html( $price, $product ){
+
+    $price_formated = str_replace('<del>','<del>List Price: ',$price);
+
+    return str_replace( '<ins>', '', $price_formated );
+
+}
--- dist/wp-content/themes/all-mobility-wp/woocommerce/content-single-product.php
+++ dist/wp-content/themes/all-mobility-wp/woocommerce/content-single-product.php
@@ -19,7 +19,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
+global $product;
 ?>
 
 <?php
@@ -32,45 +32,484 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" class="product">
 
-	<?php
-		/**
-		 * woocommerce_before_single_product_summary hook.
-		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
-		 */
-		do_action( 'woocommerce_before_single_product_summary' );
-	?>
+	<!-- product__inner -->
+	<div class="product__inner">
 
-		<?php
-			/**
-			 * woocommerce_single_product_summary hook.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
+		<?php do_action('wb_woocommerce_single_product_summary') ?>
+
+		<!-- rate -->
+		<div class="rate">
+			<div class="rate__star">
+				<div style="width: 100%"></div>
+			</div>
+			<span class="rate__reviews">15 Reviews</span>
+		</div>
+		<!-- /rate -->
+		
+		<?php do_action('wb_slider_preview') ?>
+		
+		<!-- product__price -->
+		<div class="product__price">
+
+			<?= $product->get_price_html() ?>
+
+		</div>
+		<!-- /product__price -->
+
+		<!-- product__items -->
+		<div class="product__items">
+			<div class="product__add">
+
+				<h2 class="product__add-title">Customize & Purchase</h2>
+
+				<form action="#">
+					<select name="parameter1" id="parameter1">
+						<option value="0">customizable parameter</option>
+						<option value="1">parameter1</option>
+						<option value="2">parameter2</option>
+						<option value="3">parameter3</option>
+						<option value="4">parameter4</option>
+					</select>
+					<select name="parameter2" id="parameter2">
+						<option value="0">customizable parameter</option>
+						<option value="1">parameter1</option>
+						<option value="2">parameter2</option>
+						<option value="3">parameter3</option>
+						<option value="4">parameter4</option>
+					</select>
+					<select name="parameter3" id="parameter3">
+						<option value="0">customizable parameter</option>
+						<option value="1">parameter1</option>
+						<option value="2">parameter2</option>
+						<option value="3">parameter3</option>
+						<option value="4">parameter4</option>
+					</select>
+					<select name="parameter4" id="parameter4">
+						<option value="0">customizable parameter</option>
+						<option value="1">parameter1</option>
+						<option value="2">parameter2</option>
+						<option value="3">parameter3</option>
+						<option value="4">parameter4</option>
+						<option value="4">parameter4</option>
+						<option value="4">parameter4</option>
+						<option value="4">parameter4</option>
+						<option value="4">parameter4</option>
+					</select>
+					<button type="submit" class="btn btn_2 btn_img-left">
+
+                                    <span>
+
+                                        <img src="img/cart.png" width="30" height="26px" alt="">
+
+                                        add to cart</span>
+
+					</button>
+				</form>
+
+			</div>
+			<div>
+
+				<!-- advantages -->
+				<div class="advantages advantages_2">
+
+					<ul class="advantages__list">
+						<li>
+                                <span>
+                                    <img src="img/10938-200.png" width="32" height="32" alt="">
+                                </span>
+							30-days Money Back
+							Guarantee</li>
+						<li>
+                                <span>
+                                    <img src="img/Credit_Card_Payment_Safe_Secure_Shopping_Ecommerce_Pay-512.png" width="46" height="46" alt="">
+                                </span>
+							Safe & Secure
+							Online Payments</li>
+						<li>
+                                <span>
+                                    <img src="img/Ecommerce-Free-Shipping-icon.png" width="38" height="30" alt="">
+                                </span>
+							Free Shipping
+							over $50</li>
+						<li>
+                                <span>
+                                     <img src="img/icon-innovative-research.png" width="30" height="35" alt="">
+                                </span>
+							Expert Support
+							at Your Service</li>
+						<li>
+                                <span>
+                                    <img src="img/MAW_icon-vector-blue_14_geo_125x125.png" width="23" height="36" alt="">
+                                </span>
+							Local Stores
+							Near You</li>
+					</ul>
+
+				</div>
+				<!-- /advantages -->
+
+			</div>
+		</div>
+		<!-- /product__items -->
+
+	</div>
+	<!-- /product__inner -->
+
+	<!-- product__wrap -->
+	<div class="product__wrap">
+
+		<!-- product__description -->
+		<div class="product__description product__description_pic">
+
+			<div>
+
+				<h2 class="site__title site__title_2"><?= get_the_title() ?></h2>
+				<?= get_the_content() ?>
+
+			</div>
+			<div>
+
+				<div class="product__pic">
+					<img src="pic/scooter.jpg" width="305" height="294" alt="">
+				</div>
+
+			</div>
 
 
-	<?php
-		/**
-		 * woocommerce_after_single_product_summary hook.
-		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_upsell_display - 15
-		 * @hooked woocommerce_output_related_products - 20
-		 */
-		do_action( 'woocommerce_after_single_product_summary' );
-	?>
+		</div>
+		<!-- /product__description -->
 
-	<meta itemprop="url" content="<?php the_permalink(); ?>" />
+		<!-- product__download -->
+		<div class="product__download">
+			<a href="#">Download Manual</a>
+		</div>
+		<!-- /product__download -->
 
-</div><?php the_ID(); ?> 
+		<!-- site__content__wrap -->
+		<div class="site__content__wrap">
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+			<!-- site__inner -->
+			<div class="site__inner">
+
+				<?php if( have_rows('technical_specifications_block') ): ?>
+							<h2 class="site__title site__title_2">Technical Specifications</h2>
+								<!-- product__specification -->
+								<div class="product__specification">
+				         <?php    while ( have_rows('technical_specifications_block') ) : the_row(); ?>
+
+										 <dl>
+											 <dt><?= get_sub_field('haracteristic') ?></dt>
+											 <dd><?= get_sub_field('value') ?></dd>
+										 </dl>
+
+				                <?php
+
+				            endwhile; ?>
+							</div>
+				        <?php endif; ?>
+
+				<!-- product__description -->
+				<div class="product__description">
+
+					<div>
+
+						<h2 class="site__title site__title_2">Shipping and Returns</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+					</div>
+
+				</div>
+				<!-- /product__description -->
+
+				<h2 class="site__title site__title_2">Add-ons and Enhancements:</h2>
+
+			</div>
+			<!-- /site__inner -->
+
+			<!-- site__aside -->
+			<aside class="site__aside">
+
+				<!-- resolve -->
+				<div class="resolve">
+
+					<svg width="58px" height="45px" viewBox="0 0 58 45" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+						<!-- Generator: Sketch 43.1 (39012) - http://www.bohemiancoding.com/sketch -->
+						<desc>Created with Sketch.</desc>
+						<defs></defs>
+						<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+							<g id="main-bg" transform="translate(-1230.000000, -1102.000000)">
+								<g id="Page-1-Copy" transform="translate(1230.000000, 1101.000000)">
+									<g id="Group-3" transform="translate(0.000000, 0.727200)" fill="#FEFEFE">
+										<path d="M18.3934,0.2917 C23.6914,0.2937 28.9904,0.2887 34.2884,0.2947 C41.1274,0.3017 46.9454,2.6957 51.5494,7.8057 C54.4294,11.0017 56.1344,14.7537 56.8904,19.0137 C58.5634,28.4407 53.6324,38.0687 45.3684,42.3757 C40.5144,44.9047 35.4094,45.6417 30.0864,44.4307 C21.8244,42.5497 16.3644,37.4287 13.6304,29.4997 C12.9154,27.4247 12.8514,25.1117 12.6094,22.8947 C12.4824,21.7357 13.2294,20.9997 14.2284,21.0047 C15.1194,21.0087 15.8954,21.7797 15.8874,22.8387 C15.8694,25.2227 16.2864,27.5217 17.2164,29.7027 C19.7394,35.6147 24.0484,39.5197 30.3414,41.0787 C38.4224,43.0807 46.0284,39.7877 50.4814,33.5157 C57.3864,23.7897 53.3864,9.8517 42.3744,5.1827 C39.8524,4.1137 37.2204,3.5997 34.5044,3.5937 C23.3464,3.5687 13.3414,3.5877 2.1834,3.5667 C1.6154,3.5657 0.8804,3.5177 0.5264,3.1767 C0.1524,2.8147 -0.0636,2.0477 0.0164,1.5117 C0.1344,0.7197 0.8594,0.3407 1.6534,0.2927 C2.3524,0.2497 3.0554,0.2897 3.7564,0.2897 C9.0204,0.2907 13.1304,0.2897 18.3934,0.2917" ></path>
+									</g>
+									<path d="M22.0797,8.0028 C26.3947,8.0028 30.7107,8.0428 35.0247,7.9938 C41.5647,7.9188 47.5097,12.2548 49.5207,18.4628 C52.2277,26.8158 47.8817,35.6008 39.2977,38.1738 C31.0177,40.6558 22.5577,36.0088 20.1507,27.9228 C19.7557,26.5948 19.5847,25.1888 19.4267,23.8048 C19.2947,22.6418 20.0437,21.7728 21.0287,21.7378 C22.0047,21.7028 22.6217,22.4748 22.7207,23.7288 C23.1287,28.9028 25.6187,32.6548 30.4627,34.6478 C36.2867,37.0448 43.1057,34.3208 45.8187,28.6128 C48.9057,22.1168 45.6707,14.2568 38.8587,11.9458 C37.4967,11.4838 35.9727,11.3408 34.5217,11.3278 C27.4007,11.2648 20.2787,11.2938 13.1567,11.2928 C11.2997,11.2918 8.5947,11.3288 6.7377,11.2938 C5.4917,11.2708 4.6737,10.4178 4.8227,9.4088 C4.9527,8.5208 5.5807,8.0218 6.6077,8.0208 C11.4827,8.0168 17.2047,8.0188 22.0797,8.0188 L22.0797,8.0028 Z" fill="#FEFEFE"></path>
+									<path d="M24.9073,14.826 C28.0993,14.826 31.2903,14.821 34.4823,14.827 C38.7653,14.836 41.9743,17.047 43.0943,21.141 C44.4283,26.017 41.4983,30.401 37.0133,31.672 C33.1003,32.78 28.1693,30.566 26.6733,25.814 C26.3993,24.944 26.4173,23.954 26.4483,23.024 C26.4793,22.12 27.2213,21.771 27.9943,21.762 C28.7393,21.754 29.3433,22.155 29.5223,22.958 C29.6523,23.538 29.7203,24.132 29.8373,24.715 C30.2873,26.948 32.7923,28.804 35.1423,28.654 C37.5993,28.496 39.8513,26.386 40.0273,24.077 C40.2913,20.587 38.0453,18.15 34.5253,18.143 C27.8973,18.131 21.2683,18.137 14.6403,18.132 C13.2483,18.131 12.5753,17.574 12.5973,16.461 C12.6193,15.358 13.3583,14.832 14.7033,14.841 C18.1043,14.865 21.5063,14.849 24.9073,14.849 L24.9073,14.826" id="Fill-6" fill="#F37820"></path>
+								</g>
+							</g>
+						</g>
+					</svg>
+
+					<h2 class="resolve__title">
+						Questions?
+						Concerns?
+					</h2>
+
+					<div class="resolve__call">
+						<span>Call now:</span>
+						<a href="#">1-800-555-9999</a>
+					</div>
+
+					<a href="#" class="btn btn_4">
+						CONTACT US
+					</a>
+
+				</div>
+				<!-- /resolve -->
+
+			</aside>
+			<!-- /site__aside -->
+
+		</div>
+		<!-- /site__content__wrap -->
+
+	</div>
+	<!-- /product__wrap -->
+
+	<!-- featured-products -->
+	<div class="featured-products">
+
+		<div class="swiper-container">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide">
+
+					<!-- featured-product -->
+					<a href="#" class="featured-product">
+						<h2 class="featured-product__title">Product Name </h2>
+
+						<div class="featured-product__pic" style="background-image: url(pic/lift-chairs.png)">
+						</div>
+
+						<div class="featured-product__footer">
+							<span class="featured-product__price"><del>$1,800.00</del> $1,350.00</span>
+							<span class="btn btn_6">SEE MORE</span>
+						</div>
+
+					</a>
+					<!-- /featured-product -->
+
+				</div>
+				<div class="swiper-slide">
+
+					<!-- featured-product -->
+					<a href="#" class="featured-product">
+						<h2 class="featured-product__title">Product Name </h2>
+
+						<div class="featured-product__pic" style="background-image: url(pic/lift-chairs.png)">
+						</div>
+
+						<div class="featured-product__footer">
+							<span class="featured-product__price"><del>$1,800.00</del> $1,350.00</span>
+							<span class="btn btn_6">SEE MORE</span>
+						</div>
+
+					</a>
+					<!-- /featured-product -->
+
+				</div>
+				<div class="swiper-slide">
+
+					<!-- featured-product -->
+					<a href="#" class="featured-product">
+						<h2 class="featured-product__title">Product Name </h2>
+
+						<div class="featured-product__pic" style="background-image: url(pic/lift-chairs.png)">
+						</div>
+
+						<div class="featured-product__footer">
+							<span class="featured-product__price"><del>$1,800.00</del> $1,350.00</span>
+							<span class="btn btn_6">SEE MORE</span>
+						</div>
+
+					</a>
+					<!-- /featured-product -->
+
+				</div>
+				<div class="swiper-slide">
+
+					<!-- featured-product -->
+					<a href="#" class="featured-product">
+						<h2 class="featured-product__title">Product Name </h2>
+
+						<div class="featured-product__pic" style="background-image: url(pic/lift-chairs.png)">
+						</div>
+
+						<div class="featured-product__footer">
+							<span class="featured-product__price"><del>$1,800.00</del> $1,350.00</span>
+							<span class="btn btn_6">SEE MORE</span>
+						</div>
+
+					</a>
+					<!-- /featured-product -->
+
+				</div>
+				<div class="swiper-slide">
+
+					<!-- featured-product -->
+					<a href="#" class="featured-product">
+						<h2 class="featured-product__title">Product Name </h2>
+
+						<div class="featured-product__pic" style="background-image: url(pic/lift-chairs.png)">
+						</div>
+
+						<div class="featured-product__footer">
+							<span class="featured-product__price"><del>$1,800.00</del> $1,350.00</span>
+							<span class="btn btn_6">SEE MORE</span>
+						</div>
+
+					</a>
+					<!-- /featured-product -->
+
+				</div>
+				<div class="swiper-slide">
+
+					<!-- featured-product -->
+					<a href="#" class="featured-product">
+						<h2 class="featured-product__title">Product Name </h2>
+
+						<div class="featured-product__pic" style="background-image: url(pic/lift-chairs.png)">
+						</div>
+
+						<div class="featured-product__footer">
+							<span class="featured-product__price"><del>$1,800.00</del> $1,350.00</span>
+							<span class="btn btn_6">SEE MORE</span>
+						</div>
+
+					</a>
+					<!-- /featured-product -->
+
+				</div>
+			</div>
+
+			<div class="featured-products__controls">
+				<div class="swiper-button-prev"></div>
+				<div class="swiper-pagination"></div>
+				<div class="swiper-button-next"></div>
+			</div>
+
+		</div>
+
+	</div>
+	<!-- /featured-products -->
+
+	<!-- reviews -->
+	<div class="reviews">
+
+		<h2 class="site__title site__title_2">Reviews (3)</h2>
+
+		<!-- reviews__items -->
+		<div class="reviews__items">
+
+			<!-- reviews__single -->
+			<div class="reviews__single">
+
+				<!-- reviews__head -->
+				<div class="reviews__head">
+
+					<div class="reviews__pic">
+						<img src="pic/userpic.jpg" width="74" height="74" alt="">
+					</div>
+
+					<div>
+						<h2 class="reviews__name">Jonathan Defoe,</h2>
+						<h3 class="reviews__place">Stamford, CA</h3>
+
+						<!-- rate -->
+						<div class="rate">
+							<div class="rate__star">
+								<div style="width: 100%"></div>
+							</div>
+						</div>
+						<!-- /rate -->
+
+					</div>
+
+				</div>
+				<!-- /reviews__head -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+			</div>
+			<!-- /reviews__single -->
+
+			<!-- reviews__single -->
+			<div class="reviews__single">
+
+				<!-- reviews__head -->
+				<div class="reviews__head">
+
+					<div class="reviews__pic">
+						<img src="pic/userpic.jpg" width="74" height="74" alt="">
+					</div>
+
+					<div>
+						<h2 class="reviews__name">Jonathan Defoe,</h2>
+						<h3 class="reviews__place">Stamford, CA</h3>
+
+						<!-- rate -->
+						<div class="rate">
+							<div class="rate__star">
+								<div style="width: 100%"></div>
+							</div>
+						</div>
+						<!-- /rate -->
+
+					</div>
+
+				</div>
+				<!-- /reviews__head -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+			</div>
+			<!-- /reviews__single -->
+
+			<!-- reviews__single -->
+			<div class="reviews__single">
+
+				<!-- reviews__head -->
+				<div class="reviews__head">
+
+					<div class="reviews__pic">
+						<img src="pic/userpic.jpg" width="74" height="74" alt="">
+					</div>
+
+					<div>
+						<h2 class="reviews__name">Jonathan Defoe,</h2>
+						<h3 class="reviews__place">Stamford, CA</h3>
+
+						<!-- rate -->
+						<div class="rate">
+							<div class="rate__star">
+								<div style="width: 100%"></div>
+							</div>
+						</div>
+						<!-- /rate -->
+
+					</div>
+
+				</div>
+				<!-- /reviews__head -->
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+
+			</div>
+			<!-- /reviews__single -->
+
+		</div>
+		<!-- /reviews__items -->
+
+	</div>
+	<!-- /reviews -->
+
+</div>
+
